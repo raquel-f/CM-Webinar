9e722df8c19fa27fe1438129340c8755e3e5f794
RFigas <ricasmf@gmail.com>
2021-11-03 16:18:32 +0000

    Inicial List stuff

--- app/src/main/java/com/raquel/todoapp/MainActivity.java
+++ app/src/main/java/com/raquel/todoapp/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.mainFrame_layout,TaskFragment.class,null)
+                .commit();
+
     }
 }
---
+++ app/src/main/java/com/raquel/todoapp/MyTaskRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.raquel.todoapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.raquel.todoapp.databinding.FragmentTaskBinding;
+import com.raquel.todoapp.viewmodel.Task;
+
+import java.util.List;
+
+/**
+ *
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Task> mValues;
+
+    public MyTaskRecyclerViewAdapter(List<Task> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+    return new ViewHolder(FragmentTaskBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mTitleView.setText(mValues.get(position).getTitle());
+        holder.mDateView.setText(mValues.get(position).getEndDate().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mTitleView;
+        public final TextView mDateView;
+        public Task mItem;
+
+    public ViewHolder(FragmentTaskBinding binding) {
+      super(binding.getRoot());
+      mTitleView = binding.taskTitle;
+      mDateView = binding.taskDate;
+    }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mDateView.getText() + "'";
+        }
+    }
+}
