60f1ab81a229502ded2ac2eff59c95e0b6e2c738
RFigas <ricasmf@gmail.com>
2021-11-05 16:31:43 +0000

    edit task

--- app/src/main/java/com/raquel/todoapp/fragments/MyTaskRecyclerViewAdapter.java
+++ app/src/main/java/com/raquel/todoapp/fragments/MyTaskRecyclerViewAdapter.java
@@ -1,13 +1,16 @@
 package com.raquel.todoapp.fragments;
 
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.icu.text.SimpleDateFormat;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.raquel.todoapp.FragmentSwitcher;
 import com.raquel.todoapp.databinding.FragmentTaskBinding;
 import com.raquel.todoapp.viewmodel.Task;
 
@@ -16,22 +19,23 @@ import java.util.Date;
 import java.util.Locale;
 
 /**
- *
  * TODO: Replace the implementation with code for your data type.
  */
 public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecyclerViewAdapter.ViewHolder> {
 
     private final List<Task> mValues;
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+    private FragmentSwitcher fragmentSwitcher;
 
-    public MyTaskRecyclerViewAdapter(List<Task> tasks) {
+    public MyTaskRecyclerViewAdapter(List<Task> tasks, FragmentSwitcher fragmentSwitcher) {
         mValues = tasks;
+        this.fragmentSwitcher = fragmentSwitcher;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-    return new ViewHolder(FragmentTaskBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+        return new ViewHolder(FragmentTaskBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
 
     }
 
@@ -40,11 +44,29 @@ public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecycl
         holder.mItem = mValues.get(position);
         holder.mTitleView.setText(mValues.get(position).getTitle());
         holder.mDateView.setText(dateFormat.format(mValues.get(position).getEndDate()));
+
+        //add click listener to each list item
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Task "+holder.getAbsoluteAdapterPosition());
+            }
+        });
+
+        // TODO set hold listener
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                fragmentSwitcher.switchEditTask(holder.mItem);
+                return false;
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        if(mValues == null){
+        if (mValues == null) {
             return 0;
         }
         return mValues.size();
@@ -55,11 +77,11 @@ public class MyTaskRecyclerViewAdapter extends RecyclerView.Adapter<MyTaskRecycl
         public final TextView mDateView;
         public Task mItem;
 
-    public ViewHolder(FragmentTaskBinding binding) {
-      super(binding.getRoot());
-      mTitleView = binding.taskTitle;
-      mDateView = binding.taskDate;
-    }
+        public ViewHolder(FragmentTaskBinding binding) {
+            super(binding.getRoot());
+            mTitleView = binding.taskTitle;
+            mDateView = binding.taskDate;
+        }
 
         @Override
         public String toString() {
